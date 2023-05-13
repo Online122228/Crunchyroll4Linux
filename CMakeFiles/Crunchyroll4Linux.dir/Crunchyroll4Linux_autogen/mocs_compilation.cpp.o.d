db23e11a2d5fbea6f7a31e678a09fdcd15bb15a6
Online122228 <pemtimdukaj@hotmail.com>
2023-05-13 15:07:25 +0200

    removed unnecessary files

--- CMakeFiles/Crunchyroll4Linux.dir/Crunchyroll4Linux_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Crunchyroll4Linux.dir/Crunchyroll4Linux_autogen/mocs_compilation.cpp.o: \
- /home/leart/CLionProjects/Crunchyroll4Linux/Crunchyroll4Linux_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
